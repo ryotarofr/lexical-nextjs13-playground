a1b0fd484f3c9859ff2a0285e66a46c4f8ef7f6f
ryotarofr <ryoryo.fr0608@icloud.com>
2023-10-01 14:43:58 +0900

    init

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      height: {
+        "38": "38px"
+      }
+    },
+  },
+  plugins: [],
+};
